e0be25d486fd62be8e177411880e4bc60b622964
Christine Xiang <yutingcxiang@gmail.com>
2018-12-28 16:09:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string){
+  if (string.length < 2){
+    return string
+  } else {
+    return reverseString(string.substring(1)) + string[0]
+  }
+}
+
+function isPalindrome(string){
+  if (string === reverseString(string)) {
+    return true;
+  } else {
+    return false;
+  }
+}
